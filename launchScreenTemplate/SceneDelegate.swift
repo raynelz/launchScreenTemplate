c87943bbb0d0333261f921e22df63f2354ff109d
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-08-16 11:08:07 +0300

    final commit

--- launchScreenTemplate/SceneDelegate.swift
+++ launchScreenTemplate/SceneDelegate.swift
@@ -2,15 +2,13 @@
 //  SceneDelegate.swift
 //  launchScreenTemplate
 //
-//  Created by Захар Литвинчук on 08.08.2023.
+//  Created by "Hello, Swift!" on 08.08.2023.
 //
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -46,7 +44,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
--- launchScreenTemplate/ViewController.swift
+++ launchScreenTemplate/ViewController.swift
@@ -2,18 +2,14 @@
 //  ViewController.swift
 //  launchScreenTemplate
 //
-//  Created by Захар Литвинчук on 08.08.2023.
+//  Created by "Hello, Swift!" on 08.08.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
